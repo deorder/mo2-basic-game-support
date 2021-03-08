8c42081f16d9c05566ec65a8fe7af9dae911c403
Deorder <31899960+deorder@users.noreply.github.com>
2021-03-08 19:37:30 +0100

    Removed unused code

--- plugins/basic_games/games/game_assettocorsa.py
+++ plugins/basic_games/games/game_assettocorsa.py
@@ -1,10 +1,8 @@
 from ..basic_game import BasicGame
-from ..basic_features import BasicGameSaveGameInfo
 
-import mobase
-import os
 
 class AssettoCorsaGame(BasicGame):
+
     Name = "Assetto Corsa Support Plugin"
     Author = "Deorder"
     Version = "0.0.1"
@@ -15,7 +13,3 @@ class AssettoCorsaGame(BasicGame):
     GameDataPath = r""
     GameSteamId = 244210
     GameDocumentsDirectory = "%DOCUMENTS%/Assetto Corsa"
-
-    def init(self, organizer: mobase.IOrganizer):
-        super().init(organizer)
-        return True
