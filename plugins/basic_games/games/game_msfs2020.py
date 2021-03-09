57634ff3892dde98f9cfb03dcb3210f8178ec14f
Deorder <31899960+deorder@users.noreply.github.com>
2021-03-09 21:31:10 +0100

    MSFS2020: Will now look up the pacakges directory

--- plugins/basic_games/games/game_msfs2020.py
+++ plugins/basic_games/games/game_msfs2020.py
@@ -1,5 +1,11 @@
 from ..basic_game import BasicGame
 
+from PyQt5.QtCore import QDir
+
+import mobase
+import os
+import re
+
 
 class MSFS2020Game(BasicGame):
 
@@ -10,5 +16,16 @@ class MSFS2020Game(BasicGame):
     GameName = "Microsoft Flight Simulator 2020"
     GameShortName = "msfs2020"
     GameBinary = r"FlightSimulator.exe"
-    GameDataPath = r"%USERPROFILE%\AppData\Roaming\Microsoft Flight Simulator\Packages"
+    GameDataPath = r"%USERPROFILE%\AppData\Roaming\Microsoft Flight Simulator\Packages\Community"
     GameSteamId = [1250410]
+
+    def dataDirectory(self) -> QDir:
+        # Find and use package path specified in Asobo engine options        
+        AppDataPath = os.path.expandvars(r"%APPDATA%\Microsoft Flight Simulator")
+        UserCfgPath = os.path.join(AppDataPath, "UserCfg.opt")
+        InstalledPackagesPathPattern = re.compile("InstalledPackagesPath\s*=\s*\"(.*)\"", re.IGNORECASE)
+        with open(UserCfgPath, newline='') as f:
+            for _, line in enumerate(f):
+                for match in re.finditer(InstalledPackagesPathPattern, line):
+                    return QDir(os.path.join(match.group(), "Community"))
+        return QDir(os.path.join(AppDataPath, "Packages", "Community"))
